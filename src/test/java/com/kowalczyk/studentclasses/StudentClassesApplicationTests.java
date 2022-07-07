3e174a52334755a5706db9de6d322f68cc3f7815
maleksandrowicz93 <mateusz.aleksandrowicz93@gmail.com>
2022-07-07 16:33:52 +0200

    create project

---
+++ src/test/java/com/kowalczyk/studentclasses/StudentClassesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kowalczyk.studentclasses;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentClassesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
