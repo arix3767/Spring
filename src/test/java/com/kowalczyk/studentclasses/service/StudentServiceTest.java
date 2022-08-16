837c1b6030442ff5e50be5b2f0af3661e65a9e1e
Jan <janek0619@mail.com>
2022-08-16 18:35:55 +0200

    unit tests explained Mockito

--- src/test/java/com/kowalczyk/studentclasses/service/StudentServiceTest.java
+++ src/test/java/com/kowalczyk/studentclasses/service/StudentServiceTest.java
@@ -1,23 +1,22 @@
 package com.kowalczyk.studentclasses.service;
 
-import com.kowalczyk.studentclasses.converters.StudentDtoToStudentConverter;
 import com.kowalczyk.studentclasses.converters.StudentToStudentDtoConverter;
 import com.kowalczyk.studentclasses.dto.StudentDto;
 import com.kowalczyk.studentclasses.enums.Messages;
 import com.kowalczyk.studentclasses.model.Student;
 import com.kowalczyk.studentclasses.repository.StudentRepository;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.ResponseEntity;
 
 import java.util.Collections;
-import java.util.List;
 import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 class StudentServiceTest {
@@ -38,12 +37,29 @@ class StudentServiceTest {
     @DisplayName("Verify if get all works correctly")
     void getAll() {
         // given
-        Mockito.when(studentRepository.findAll()).thenReturn(Map.of(EMAIL, buildStudent()));
+        Map<String, Student> expectedStudents = Collections.singletonMap(EMAIL, buildStudent());
+        Mockito.when(studentRepository.findAll()).thenReturn(expectedStudents);
         // when
         Map<String, StudentDto> students = studentService.getAll();
         // then
-        // when + then -> w jednej sekcji expect
-        Assertions.assertEquals(1, students.size());
+        assertEquals(expectedStudents.size(), students.size());
+        students.forEach((email, studentDto) -> {
+            Student expectedStudentDto = expectedStudents.get(email);
+            assertEquals(expectedStudentDto.getEmail(), studentDto.getEmail());
+            assertEquals(expectedStudentDto.getName(), studentDto.getName());
+            assertEquals(expectedStudentDto.getTeacher(), studentDto.getTeacher());
+            assertEquals(expectedStudentDto.getRate(), studentDto.getRate());
+        });
+    }
+
+    @Test
+    void addAnyStudent() {
+        // given
+        Mockito.when(studentRepository.existsByEmail(Mockito.anyString())).thenReturn(false);
+        // when
+        studentService.addStudent(Mockito.mock(StudentDto.class));
+        // then
+        Mockito.verify(studentRepository).save(Mockito.any(Student.class));
     }
 
     @Test
@@ -52,9 +68,10 @@ class StudentServiceTest {
         Mockito.when(studentRepository.existsByEmail(EMAIL)).thenReturn(false);
         Student student = buildStudent();
         // when
-        studentService.addStudent(StudentToStudentDtoConverter.INSTANCE.convert(student));
+        String message = studentService.addStudent(StudentToStudentDtoConverter.INSTANCE.convert(student));
         // then
-        Mockito.verify(studentRepository, Mockito.times(1)).save(Mockito.any(Student.class));
+        Mockito.verify(studentRepository).save(student);
+        assertEquals(Messages.STUDENT_ADD_SUCCESS.getText(), message);
     }
 
     @Test
@@ -69,6 +86,15 @@ class StudentServiceTest {
         Mockito.verify(studentRepository, Mockito.times(1)).save(Mockito.any(Student.class));
     }
 
+    @Test
+    void findStudent() {
+
+    }
+
+    @Test
+    void deleteStudent() {
+
+    }
 
     private Student buildStudent() {
         return Student.builder()
