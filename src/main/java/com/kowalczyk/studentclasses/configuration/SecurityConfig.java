5cfd5b25a7b77483257a3d2145412d6e82cd18bc
Jan <janek0619@mail.com>
2022-09-06 18:34:33 +0200

    add Authentication

--- src/main/java/com/kowalczyk/studentclasses/configuration/SecurityConfig.java
+++ src/main/java/com/kowalczyk/studentclasses/configuration/SecurityConfig.java
@@ -4,12 +4,16 @@ import com.kowalczyk.studentclasses.entity.UserData;
 import com.kowalczyk.studentclasses.exception.UserNotFoundException;
 import com.kowalczyk.studentclasses.repository.UserRepository;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 import java.util.Collections;
 
@@ -20,6 +24,25 @@ public class SecurityConfig {
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder(11);
     }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .csrf().disable()
+                .httpBasic()
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .headers()
+                .frameOptions().disable()
+                .and()
+                .authorizeRequests()
+                .antMatchers(HttpMethod.POST, "/Student").anonymous()
+                .anyRequest().authenticated()
+                .and().build();
+    }
+
     @Bean
     public UserDetailsService userDetailsService(UserRepository userRepository) {
         return username -> userRepository.findByEmail(username)
