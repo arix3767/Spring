4f61969def9e164d3d70a683091936e51e7b3350
maleksandrowicz93 <mateusz.aleksandrowicz93@gmail.com>
2022-10-05 11:44:59 +0200

    add missing repository annotations

--- src/main/java/com/kowalczyk/studentclasses/repository/ClassRoomRepository.java
+++ src/main/java/com/kowalczyk/studentclasses/repository/ClassRoomRepository.java
@@ -2,6 +2,8 @@ package com.kowalczyk.studentclasses.repository;
 
 import com.kowalczyk.studentclasses.entity.ClassRoom;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ClassRoomRepository extends JpaRepository<ClassRoom, Long> {
 }
--- src/main/java/com/kowalczyk/studentclasses/repository/UserRepository.java
+++ src/main/java/com/kowalczyk/studentclasses/repository/UserRepository.java
@@ -2,9 +2,11 @@ package com.kowalczyk.studentclasses.repository;
 
 import com.kowalczyk.studentclasses.entity.UserData;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends JpaRepository<UserData, Long> {
 
     Optional<UserData> findByEmail(String email);
