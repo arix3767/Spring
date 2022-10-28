b51a815a200d599a77d680d34a1c8b2c977bf211
arix3767 <103655172+arix3767@users.noreply.github.com>
2022-10-28 17:28:33 +0200

    extend exception handler

--- src/main/java/com/kowalczyk/studentclasses/handler/BusinessExceptionHandler.java
+++ src/main/java/com/kowalczyk/studentclasses/handler/BusinessExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.kowalczyk.studentclasses.handler;
 
+import com.kowalczyk.studentclasses.exception.AddressException;
 import com.kowalczyk.studentclasses.exception.StudentException;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
@@ -9,10 +10,16 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 @Order(Ordered.HIGHEST_PRECEDENCE)
-public class StudentExceptionHandler {
+public class BusinessExceptionHandler {
 
     @ExceptionHandler(StudentException.class)
-    public ResponseEntity<String> studentAlreadyExist(StudentException e) {
+    public ResponseEntity<String> handleStudentException(StudentException e) {
         return ResponseEntity.badRequest().body(e.getMessage());
     }
+
+    @ExceptionHandler(AddressException.class)
+    public ResponseEntity<String> handleAddressException(AddressException e) {
+        return ResponseEntity.badRequest().body(e.getMessage());
+    }
+
 }
