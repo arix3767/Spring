13b8035acadccb1009837633926730515a76b278
arix3767 <103655172+arix3767@users.noreply.github.com>
2022-12-30 17:28:26 +0100

    create profiles in configuration

--- src/main/java/com/kowalczyk/studentclasses/StudentClassesApplication.java
+++ src/main/java/com/kowalczyk/studentclasses/StudentClassesApplication.java
@@ -2,8 +2,10 @@ package com.kowalczyk.studentclasses;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "com.kowalczyk.studentclasses.repository")
 public class StudentClassesApplication {
 
 	public static void main(String[] args) {
